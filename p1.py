77208454e1b3ee21cf2b60341337b4d5899593a1
David Viñas <davidvimo1@hotmail.com>
2021-09-17 17:02:05 +0200

    'clase_1'

--- p1.py
+++ p1.py
@@ -1,3 +1,20 @@
-print("Bob Dylan")
 
-print("The times they are a-changin")
+class CS:
+    def __init__(self):
+        self.index = []
+    
+    def añadir(self, lista):
+        return self.index.append(lista)
+
+    #Devuelve la dimensión del complejo símplice
+    def dimension(self):
+        max = 0
+        for i in self.index:
+            if (len(i) > max):
+                max = len(i)
+        return max
+simplice_prueba = CS()
+simplice_prueba.añadir(["1"])
+simplice_prueba.añadir(["2"])
+simplice_prueba.añadir(["1", "2", "3", "4"])
+print(simplice_prueba.dimension())
